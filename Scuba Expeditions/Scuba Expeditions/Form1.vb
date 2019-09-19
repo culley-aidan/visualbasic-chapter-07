23672ebbe5fe5e9eed50286b0aaa2acf6e0485af
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-19 08:59:31 -0500

    Created the class variables

--- Scuba Expeditions/Scuba Expeditions/Form1.vb
+++ Scuba Expeditions/Scuba Expeditions/Form1.vb
@@ -6,4 +6,15 @@
 Option Strict On
 Public Class frmScuba
 
+    ' Class variables
+    Private _intFourHour As Integer = 4
+    Private _intSixHour As Integer = 6
+    Private _intEightHour As Integer = 8
+    Private _strDive1 As String = "Great Barrier Reef"
+    Private _strDive2 As String = "Tiger Shark"
+    Private _strDive3 As String = "Fathom Chasm"
+    Private _strDive4 As String = "Great Blue Chasm"
+    Private _strDive5 As String = "Rainbow reef"
+    Private _strDive6 As String = "Namena Sunken Ship"
+
 End Class
