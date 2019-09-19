5319fbf58d51fdd5034cebba6763399f82a4a9c6
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-19 08:55:41 -0500

    started working on the code

--- Scuba Expeditions/Scuba Expeditions/Form1.vb
+++ Scuba Expeditions/Scuba Expeditions/Form1.vb
@@ -1,3 +1,8 @@
-﻿Public Class frmScuba
+﻿' Program Name: Scuba Expeditions Application
+' Author:       Aidan J Culley
+' Date:         September 19th, 2019
+' Purpose:      The Scuba Epeditions app determins the location and scuba dives available and calculates the total dive team cost.
+
+Public Class frmScuba
 
 End Class
