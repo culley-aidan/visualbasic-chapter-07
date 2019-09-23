b044953cd1bb6024b5367c1f7a2aba452ff8b898
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-23 10:25:11 -0500

    fixed

--- IMAX Theatre/IMAX Theatre/Form1.vb
+++ IMAX Theatre/IMAX Theatre/Form1.vb
@@ -3,6 +3,9 @@
 ' Date:         September 23rd, 2019
 ' Purpose:      This application calculates total cost of tickets based off of IMAX show
 
-Option Strict On Ons
+Option Strict On On
 Public Class frmIMAX
+    Private Sub CboShow_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboShow.SelectedIndexChanged
+
+    End Sub
 End Class
