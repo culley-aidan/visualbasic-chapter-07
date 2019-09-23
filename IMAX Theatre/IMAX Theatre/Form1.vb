3f0c0df4185b1695b40e89501a08f0410e2053a5
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-23 10:23:55 -0500

    started working on logic

--- IMAX Theatre/IMAX Theatre/Form1.vb
+++ IMAX Theatre/IMAX Theatre/Form1.vb
@@ -1,2 +1,8 @@
-﻿Public Class frmIMAX
+﻿' Program Name: IMAX Theatre Tickets
+' Author:       Aidan J Culley
+' Date:         September 23rd, 2019
+' Purpose:      This application calculates total cost of tickets based off of IMAX show
+
+
+Public Class frmIMAX
 End Class
