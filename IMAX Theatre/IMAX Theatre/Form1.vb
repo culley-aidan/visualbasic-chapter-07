8e95bee635f9ed3dd1b521910ab4f87bbbee88be
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-23 10:25:01 -0500

    turned on strict type checking

--- IMAX Theatre/IMAX Theatre/Form1.vb
+++ IMAX Theatre/IMAX Theatre/Form1.vb
@@ -3,6 +3,6 @@
 ' Date:         September 23rd, 2019
 ' Purpose:      This application calculates total cost of tickets based off of IMAX show
 
-
+Option Strict On Ons
 Public Class frmIMAX
 End Class
