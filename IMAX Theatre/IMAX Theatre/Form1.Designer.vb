d284051bb4609fa586f1612951a2e38ba6192baa
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-23 09:08:58 -0500

    created lblTitle

--- IMAX Theatre/IMAX Theatre/Form1.Designer.vb
+++ IMAX Theatre/IMAX Theatre/Form1.Designer.vb
@@ -22,7 +22,20 @@ Partial Class frmIMAX
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.Transparent
+        Me.lblTitle.Location = New System.Drawing.Point(492, 31)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(315, 33)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "IMAX Theatre Tickets"
         '
         'frmIMAX
         '
@@ -30,11 +43,14 @@ Partial Class frmIMAX
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.IMAX_Theatre.My.Resources.Resources.imax
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(854, 451)
+        Me.Controls.Add(Me.lblTitle)
         Me.Name = "frmIMAX"
         Me.Text = "IMAX Theatre"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblTitle As Label
 End Class
