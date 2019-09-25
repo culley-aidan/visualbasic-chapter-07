9c22455526850c8847f0224ef573ba8c31c1ad80
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-25 09:51:14 -0500

    finished logic for selectedindexchanged

--- Body Mass Index Calculator/Body Mass Index Calculator/Form1.vb
+++ Body Mass Index Calculator/Body Mass Index Calculator/Form1.vb
@@ -6,6 +6,21 @@
 Option Strict On
 Public Class frmBMICalc
     Private Sub CboUnits_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboUnits.SelectedIndexChanged
+        ' updates display
+
+        lblWeight.Visible = True
+        lblHeight.Visible = True
+        txtWeight.Visible = True
+        txtHeight.Visible = True
+        btnCalculate.Visible = True
+        btnClear.Visible = True
+    End Sub
+
+    Private Sub MetricBMI()
+
+    End Sub
+
+    Private Sub ImperialBMI()
 
     End Sub
 End Class
