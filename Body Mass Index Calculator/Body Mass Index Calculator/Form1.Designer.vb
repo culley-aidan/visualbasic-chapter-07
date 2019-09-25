45342929162c69149841e333a0b5c514d90924ab
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-25 09:08:31 -0500

    added a splashscreen and background image

--- Body Mass Index Calculator/Body Mass Index Calculator/Form1.Designer.vb
+++ Body Mass Index Calculator/Body Mass Index Calculator/Form1.Designer.vb
@@ -22,17 +22,35 @@ Partial Class frmBMICalc
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.picBackground = New System.Windows.Forms.PictureBox()
+        CType(Me.picBackground, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'picBackground
+        '
+        Me.picBackground.BackgroundImage = Global.Body_Mass_Index_Calculator.My.Resources.Resources.epic
+        Me.picBackground.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picBackground.Location = New System.Drawing.Point(0, 0)
+        Me.picBackground.Name = "picBackground"
+        Me.picBackground.Size = New System.Drawing.Size(412, 450)
+        Me.picBackground.TabIndex = 0
+        Me.picBackground.TabStop = False
         '
         'frmBMICalc
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.picBackground)
+        Me.DoubleBuffered = True
         Me.Name = "frmBMICalc"
         Me.Text = "Body Mass Index Calculator"
+        CType(Me.picBackground, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents picBackground As PictureBox
 End Class
