b946f44a74abb20c5a0ab54b215455fc69782677
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-25 09:12:50 -0500

    created lblTitle and panel1

--- Body Mass Index Calculator/Body Mass Index Calculator/Form1.Designer.vb
+++ Body Mass Index Calculator/Body Mass Index Calculator/Form1.Designer.vb
@@ -23,7 +23,10 @@ Partial Class frmBMICalc
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.picBackground = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblTitle = New System.Windows.Forms.Label()
         CType(Me.picBackground, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'picBackground
@@ -36,6 +39,24 @@ Partial Class frmBMICalc
         Me.picBackground.TabIndex = 0
         Me.picBackground.TabStop = False
         '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblTitle)
+        Me.Panel1.Location = New System.Drawing.Point(410, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(390, 450)
+        Me.Panel1.TabIndex = 1
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(8, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(378, 33)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Body Mass Index Calculator"
+        '
         'frmBMICalc
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -43,14 +64,19 @@ Partial Class frmBMICalc
         Me.BackColor = System.Drawing.Color.White
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.picBackground)
         Me.DoubleBuffered = True
         Me.Name = "frmBMICalc"
         Me.Text = "Body Mass Index Calculator"
         CType(Me.picBackground, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents picBackground As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTitle As Label
 End Class
