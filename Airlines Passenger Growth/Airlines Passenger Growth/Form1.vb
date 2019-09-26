1f7b406e2afbe74ed08278f99643cd05a9aa98f9
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-26 08:57:50 -0500

    started working on the code

--- Airlines Passenger Growth/Airlines Passenger Growth/Form1.vb
+++ Airlines Passenger Growth/Airlines Passenger Growth/Form1.vb
@@ -1,3 +1,7 @@
-﻿Public Class frmGrowth
+﻿' Program Name: Factorial Calculator
+' Author:       Aidan J Culley
+' Date:         September 26th, 2019 
+' Purpose:      Predicts the future amount of airline passengers
 
+Public Class frmGrowth
 End Class
