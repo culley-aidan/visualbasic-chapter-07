f6ae65af56e8950fa51d16d94751b42c8530ca59
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-26 09:56:22 -0500

    finished

--- Airlines Passenger Growth/Airlines Passenger Growth/Form1.Designer.vb
+++ Airlines Passenger Growth/Airlines Passenger Growth/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmGrowth
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,13 +20,14 @@ Partial Class frmGrowth
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lblTitle = New System.Windows.Forms.Label()
         Me.btnCalculate = New System.Windows.Forms.Button()
         Me.btnSpecificCalculate = New System.Windows.Forms.Button()
         Me.lstGrowth = New System.Windows.Forms.ListBox()
         Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblPrediction = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblTitle
@@ -73,17 +74,30 @@ Partial Class frmGrowth
         '
         Me.lblYear.AutoSize = True
         Me.lblYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblYear.Location = New System.Drawing.Point(92, 393)
+        Me.lblYear.Location = New System.Drawing.Point(12, 383)
         Me.lblYear.Name = "lblYear"
-        Me.lblYear.Size = New System.Drawing.Size(342, 48)
+        Me.lblYear.Size = New System.Drawing.Size(97, 24)
         Me.lblYear.TabIndex = 4
-        Me.lblYear.Text = "In the year XXXX, " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "There will be 3,800,000,000 passengers"
+        Me.lblYear.Text = "In the year"
+        Me.lblYear.Visible = False
+        '
+        'lblPrediction
+        '
+        Me.lblPrediction.AutoSize = True
+        Me.lblPrediction.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrediction.Location = New System.Drawing.Point(12, 407)
+        Me.lblPrediction.Name = "lblPrediction"
+        Me.lblPrediction.Size = New System.Drawing.Size(124, 24)
+        Me.lblPrediction.TabIndex = 5
+        Me.lblPrediction.Text = "There will be "
+        Me.lblPrediction.Visible = False
         '
         'frmGrowth
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(527, 450)
+        Me.Controls.Add(Me.lblPrediction)
         Me.Controls.Add(Me.lblYear)
         Me.Controls.Add(Me.lstGrowth)
         Me.Controls.Add(Me.btnSpecificCalculate)
@@ -101,4 +115,5 @@ Partial Class frmGrowth
     Friend WithEvents btnSpecificCalculate As Button
     Friend WithEvents lstGrowth As ListBox
     Friend WithEvents lblYear As Label
+    Friend WithEvents lblPrediction As Label
 End Class
--- Airlines Passenger Growth/Airlines Passenger Growth/Form1.vb
+++ Airlines Passenger Growth/Airlines Passenger Growth/Form1.vb
@@ -22,9 +22,33 @@ Public Class frmGrowth
             intStarting += 1
             intEntries += 1
             ulongGrowth += CULng(ulongGrowth * decGrowth)
-            lstGrowth.Items.Add(String.Format("Year {0}: {1}", intStarting.ToString(), ulongGrowth.ToString()))
+            lstGrowth.Items.Add(String.Format("Year {0}: {1}", intStarting.ToString(), ulongGrowth.ToString("N").Substring(0, ulongGrowth.ToString("N").Length - 3)))
         Loop
     End Sub
 
+    Private Sub BtnSpecificCalculate_Click(sender As Object, e As EventArgs) Handles btnSpecificCalculate.Click
+        ' calculates a prediction for a certain year within 2017-2027
 
+        Dim strInputMessage As String = "Enter the year you want between (2017-2027)"
+        Dim strInputHeading As String = "Growth Prediction"
+        Dim strInput As String = InputBox(strInputMessage, strInputHeading, "")
+        Dim intFinalYear As Integer
+
+        Do Until Integer.TryParse(strInput, intFinalYear) = True And intFinalYear >= 2018 And intFinalYear <= 2027
+            strInput = InputBox(strInputMessage, strInputHeading, "")
+        Loop
+        lblYear.Visible = True
+        lblYear.Text = "In the year " & intFinalYear.ToString() & ","
+        lblPrediction.Visible = True
+        lblPrediction.Text = "There will be " & growthCalculation(intFinalYear).ToString("N") & " airline passengers"
+    End Sub
+    Function growthCalculation(ByVal intFinalYear As Integer) As ULong
+        Dim intYears As Integer = 2017
+        Dim ulongPrediction As ULong = ulong2017
+        Do Until intYears = intFinalYear
+            intYears += 1
+            ulongPrediction += CULng(ulongPrediction * decGrowth)
+        Loop
+        Return ulongPrediction
+    End Function
 End Class
