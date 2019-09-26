b0c49bc133666efcca3d84508ed63314f97fa1b1
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-26 08:56:14 -0500

    created lblYear

--- Airlines Passenger Growth/Airlines Passenger Growth/Form1.Designer.vb
+++ Airlines Passenger Growth/Airlines Passenger Growth/Form1.Designer.vb
@@ -26,6 +26,7 @@ Partial Class frmGrowth
         Me.btnCalculate = New System.Windows.Forms.Button()
         Me.btnSpecificCalculate = New System.Windows.Forms.Button()
         Me.lstGrowth = New System.Windows.Forms.ListBox()
+        Me.lblYear = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblTitle
@@ -68,11 +69,22 @@ Partial Class frmGrowth
         Me.lstGrowth.Size = New System.Drawing.Size(435, 244)
         Me.lstGrowth.TabIndex = 3
         '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYear.Location = New System.Drawing.Point(92, 393)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(342, 48)
+        Me.lblYear.TabIndex = 4
+        Me.lblYear.Text = "In the year XXXX, " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "There will be 3,800,000,000 passengers"
+        '
         'frmGrowth
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(527, 450)
+        Me.Controls.Add(Me.lblYear)
         Me.Controls.Add(Me.lstGrowth)
         Me.Controls.Add(Me.btnSpecificCalculate)
         Me.Controls.Add(Me.btnCalculate)
@@ -88,4 +100,5 @@ Partial Class frmGrowth
     Friend WithEvents btnCalculate As Button
     Friend WithEvents btnSpecificCalculate As Button
     Friend WithEvents lstGrowth As ListBox
+    Friend WithEvents lblYear As Label
 End Class
